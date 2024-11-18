e2f79956deaa82ad7f5fd2b3582aaf30ec58eec8
Laryssa Ramos <laryssaramos284@gmail.com>
2024-11-18 06:12:43 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,13 @@ public class Program {
 		 for (Seller obj : list) {
 			 System.out.println(obj);
 		 }
+		 
+		 System.out.println("\n=== TEST 3: seller findByIdAll ===");
+		 list = sellerDao.findAll();
+		 
+		 for (Seller obj : list) {
+			 System.out.println(obj);
+		 }
 	}
 	
 	
